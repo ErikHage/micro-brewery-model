b569c90a365c8c0b1cbbc342c75ce2d8fe5ef403
Erik Hage <ehage4@gmail.com>
2017-04-21 22:41:04 -0400

    Initialize Spring Boot 1.5.3 project

---
+++ src/main/java/com/tfr/microbrew/MicroBreweryModelApplication.java
@@ -0,0 +1,12 @@
+package com.tfr.microbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBreweryModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBreweryModelApplication.class, args);
+	}
+}
